6f3d26c1b20c17f75df205e75c16973d7348878b
Paul Hubbard <phubbard@pdx.edu>
2020-03-03 14:22:14 -0800

    Removed old game files

--- FinalProject/ColossalCaveAdventure/app/Main.hs
+++ FinalProject/ColossalCaveAdventure/app/Main.hs
@@ -10,9 +10,10 @@ main =  do
         putStrLn ""
         putStrLn "To play the game, type short phrases into the command line below. \nIf you type the word 'look,' the game gives you a description of your surroundings.  \nTyping 'inventory' tells you what you're carrying.  \n'Get' 'drop' and 'throw' helps you interact with objects.  \nPart of the game is trying out different commands and seeing what happens.  \nType 'help' at any time for game instructions. \n\nWould you like more instructions?"
         response <- getLine
-        if response == "road" then putStrLn ("You are next to " ++ (fst (gameMap !! (fst (snd (gameMap !! 0))))))
+        if response == "road" then putStrLn ("You are next to a " ++ (fst (gameMap !! (fst (snd (gameMap !! 0))))))
         else putStrLn (fst (gameMap !! 0))
 
 clear = putStr "\ESC[2J"
 
 gameMap = [("road", (1, 2)),("river", (0, 2)), ("house", (0, 1))]
+
