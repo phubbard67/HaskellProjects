a0e88ee34db47f34a5e5186a4adc5e314ce297ed
Paul Hubbard <phubbard@pdx.edu>
2020-03-07 15:52:00 -0800

    getting closer

--- FinalProject/ColossalCaveAdventure/app/Main.hs
+++ FinalProject/ColossalCaveAdventure/app/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import Data.Char
+import Control.Monad
+
 -- TODO: Make this a recursing loop. There is an example of how 
 --       to do so in the IO section of learn you a haskell
 --       you should also be passing in the gameMap location
@@ -9,14 +12,19 @@ main =  do
         clear
         putStrLn gameIntro
         response <- getLine
-        putStrLn (processCommand response)
+        if response /= "quit"
+                then do
+                        game
+                else return()
+
+
         
 -- clears the terminal
 clear = putStr "\ESC[2J"
 
 -- TODO: Maybe turn the game map into a type
 -- this is the map for the game
-gameMap = [("road", (1, 2)),("river", (0, 2)), ("house", (0, 1))]
+gameMap = [ (("road", "none") , (1, 2)) , (("river", "none") , (0, 3)) , (("house", "key") , (0, 1)), (("cave", "none") , (-1, -1))]
 
 -- TODO: Change this so it takes in the game map position
 --        so that you can track where the user is in the game
@@ -25,11 +33,58 @@ gameMap = [("road", (1, 2)),("river", (0, 2)), ("house", (0, 1))]
 --        based on this value. Where it says gameMap !! 0 it will read
 --        gameMap !! mapPos
 -- This command processes the user input and returns the next location
-processCommand :: String -> String
-processCommand a = if a == "help" then helpString 
-                    else if a == "road" then ("You are next to a " ++ (fst (gameMap !! (fst (snd (gameMap !! 0))))) ++ " and a " ++ (fst (gameMap !! (snd (snd (gameMap !! 0))))))
-                    else "Sorry, that command is not recognized"
+processCommand :: (String , Int) -> ((String , String) , (Int , Int))
+processCommand (a , b) = if a == "help" then ((helpString , "none") , (-1, -1))
+                    else if b == -2 then (("Sorry, that command is not recognized b = -2" , "none") , (-1, -1))
+                    else if a == "no" then 
+                                        if getMapItem (b) == "none" then ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b) ++ " and there is a " ++ getMapItem(b) ++ " in the room.") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                    else if a == "test" then (do res <- getLine)
+                    else (("Sorry, that command is not recognized" , "none") , (-1, -1))
 
+-- TODO: need to change this to a limit, like the player only gets three turns, because the recursion can't remember values
+game :: IO()
+game = do
+        response <- getLine
+        when (response /= "quit")
+                $ do
+                        let location = (processCommand (response , playerPos))
+                        putStrLn (fst ( fst (location)))
+                        response <- getLine
+                        let nextPos = (getNextLocation (response , playerPos))
+                        let nextLoc = (processCommand ("no" , nextPos))
+                        putStrLn (fst ( fst (nextLoc)))
+                        game
+                
+
+-- returns the left location string
+getMapLeft :: Int -> String
+getMapLeft a = (fst ( fst (gameMap !! (fst (snd (gameMap !! a))))))
+
+-- returns the location of the left map in the list
+getMapLeftLoc :: Int -> Int
+getMapLeftLoc a = (fst (snd (gameMap !! a)))
+
+-- returns the right map location
+getMapRight :: Int -> String
+getMapRight a = (fst (fst (gameMap !! (snd (snd (gameMap !! a))))))
+
+-- returns the right location in the maps list
+getMapRightLoc :: Int -> Int
+getMapRightLoc a = (snd (snd (gameMap !! a)))
+
+-- returns the item in the room
+getMapItem :: Int -> String
+getMapItem a = snd (fst ((gameMap !! a)))
+
+-- This holds the players position in the list
+playerPos = 0
+
+-- This returns the next list location from the string input by the user
+getNextLocation :: (String , Int) -> Int
+getNextLocation (a , b) = if (map toUpper a) == "LEFT"  then (getMapLeftLoc(b))
+                    else if (map toUpper a) == "RIGHT" then (getMapRightLoc(b))
+                    else -2
 
 -- This is just the string for help that is from https://www.amc.com/shows/halt-and-catch-fire/exclusives/colossal-cave-adventure
 helpString = "I know of places, actions, and things.  Most of my vocabulary describes\n\
@@ -62,3 +117,4 @@ gameIntro = "              Welcome to Colossal Haskell Adventure\n\
             \Part of the game is trying out different commands and seeing what happens.\n\
             \Type 'help' at any time for game instructions.\n\n\
             \Would you like more instructions?"
+
