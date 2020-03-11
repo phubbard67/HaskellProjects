7e163be45ca34dfdf11ce81410dac8aa4c2180c7
Paul Hubbard <phubbard@pdx.edu>
2020-03-10 17:34:16 -0700

    You have a somewhat playable game

--- FinalProject/ColossalCaveAdventure/app/Main.hs
+++ FinalProject/ColossalCaveAdventure/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Data.Char
 import Control.Monad
+import Data.IORef
 
 -- TODO: Make this a recursing loop. There is an example of how 
 --       to do so in the IO section of learn you a haskell
@@ -15,6 +16,7 @@ main =  do
         if response /= "quit"
                 then do
                         game
+                        main
                 else return()
 
 
@@ -24,7 +26,7 @@ clear = putStr "\ESC[2J"
 
 -- TODO: Maybe turn the game map into a type
 -- this is the map for the game
-gameMap = [ (("road", "none") , (1, 2)) , (("river", "none") , (0, 3)) , (("house", "key") , (0, 1)), (("cave", "none") , (-1, -1))]
+gameMap = [ (("road", "none") , (1, 2)) , (("river", "none") , (3, 0)) , (("house", "key") , (0, 1)), (("cave", "door") , (1, 0))]
 
 -- TODO: Change this so it takes in the game map position
 --        so that you can track where the user is in the game
@@ -33,28 +35,48 @@ gameMap = [ (("road", "none") , (1, 2)) , (("river", "none") , (0, 3)) , (("hous
 --        based on this value. Where it says gameMap !! 0 it will read
 --        gameMap !! mapPos
 -- This command processes the user input and returns the next location
-processCommand :: (String , Int) -> ((String , String) , (Int , Int))
-processCommand (a , b) = if a == "help" then ((helpString , "none") , (-1, -1))
-                    else if b == -2 then (("Sorry, that command is not recognized b = -2" , "none") , (-1, -1))
-                    else if a == "no" then 
-                                        if getMapItem (b) == "none" then ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
-                                        else ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b) ++ " and there is a " ++ getMapItem(b) ++ " in the room.") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
-                    else if a == "test" then (do res <- getLine)
-                    else (("Sorry, that command is not recognized" , "none") , (-1, -1))
+processCommand :: (String , String, Int) -> ((String , String) , (Int , Int))
+processCommand (a , c, b) 
+                        | a == "help" = ((helpString , "none") , (-1, -1))
+                        | b == -2 = (("Sorry, that command is not recognized" , "none") , (-1, -1))
+                        | a == "no"  = if getMapItem (b) == "none" then ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else if getMapItem(b) == "key" then ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b) ++ " and you have found a " ++ getMapItem(b) ++ " in the room.") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else if getMapItem(b) == "door" && c == "key" then ((("You are at the cave and have found the key! Congratulations, you now have all the gold!") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else if getMapItem(b) == "door" && c == "none" then ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b) ++ ". You see a key hole, but have no key. It must be somewhere...") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                        | otherwise = (("Sorry, that command is not recognized" , "none") , (-1, -1))
 
--- TODO: need to change this to a limit, like the player only gets three turns, because the recursion can't remember values
+                                        
+-- TODO: fix this. You keep seeing that the wrong left and right location is being shown
 game :: IO()
 game = do
-        response <- getLine
         when (response /= "quit")
                 $ do
-                        let location = (processCommand (response , playerPos))
+                        let location = (processCommand (response , "none", playerPos)) --try one
                         putStrLn (fst ( fst (location)))
+                        let inventory = (snd (fst (location))) 
                         response <- getLine
-                        let nextPos = (getNextLocation (response , playerPos))
-                        let nextLoc = (processCommand ("no" , nextPos))
+                        let nextPos = (getNextLocation (response ,  playerPos)) --try two --here is where it starts going wrong
+                        let nextLoc = (processCommand ("no" , searchInv inventory, nextPos))
                         putStrLn (fst ( fst (nextLoc)))
-                        game
+                        let inventoryOne = inventory ++ (snd (fst (nextLoc))) 
+                        response <- getLine
+                        let nextPosOne = (getNextLocation (response , nextPos)) --try three
+                        let nextLocOne = (processCommand ("no" , searchInv inventoryOne, nextPosOne))
+                        putStrLn (fst ( fst (nextLocOne)))
+                        print nextLocOne
+                        let inventoryTwo = inventoryOne ++ (snd (fst (nextLocOne)))
+                        response <- getLine
+                        let nextPosTwo = (getNextLocation (response , nextPosOne)) --try four
+                        let nextLocTwo = (processCommand ("no" , searchInv inventoryTwo, nextPosTwo))
+                        putStrLn (fst ( fst (nextLocTwo)))
+                        let inventoryThree = inventoryTwo ++ (snd (fst (nextLocTwo)))
+                        putStrLn inventoryThree
+                        putStrLn (searchInv inventoryThree)
+                        response <- getLine
+                        let nextPosThree = (getNextLocation (response, nextPosTwo)) --try five
+                        let nextLocThree = (processCommand ("no" , searchInv inventoryThree, nextPosThree))
+                        putStrLn (fst ( fst (nextLocThree)))
                 
 
 -- returns the left location string
@@ -80,41 +102,36 @@ getMapItem a = snd (fst ((gameMap !! a)))
 -- This holds the players position in the list
 playerPos = 0
 
+-- used to start the game
+response = "no"
+
+
+
+-- sets the inventory if the user has a key
+searchInv :: String -> String
+searchInv a | (filter (/= "key") [a] /= []) = "key"
+            | otherwise = "none"
+
 -- This returns the next list location from the string input by the user
-getNextLocation :: (String , Int) -> Int
+getNextLocation :: (String , Int) ->  Int
 getNextLocation (a , b) = if (map toUpper a) == "LEFT"  then (getMapLeftLoc(b))
                     else if (map toUpper a) == "RIGHT" then (getMapRightLoc(b))
                     else -2
 
 -- This is just the string for help that is from https://www.amc.com/shows/halt-and-catch-fire/exclusives/colossal-cave-adventure
-helpString = "I know of places, actions, and things.  Most of my vocabulary describes\n\
-\places and is used to move you there.  To move, try words like forest,\n\
-\building, downstream, enter, east, west, north, south, up or down.\n\
-\I know about a few special objects, like a black rod hidden in the cave.\n\
-\These objects can be manipulated using some of the action words I know.\n\
-\Usually you will need to give both the object and action words -In\n\
-\either order - but sometimes I can infer the object from the verb alone.\n\
-\Some objects also imply verbs; in particular, 'inventory' implies 'take\n\
-\inventory,' which causes me to give you a list of what you're carrying.\n\
-\The objects have side effects; for instance, the rod scares the bird.\n\
-\Usually people having trouble moving just need to try a few more words.\n\
-\Usually people trying unsuccessfully to manipulate an object are\n\
-\attempting something beyond their (or my!) capabilities and should try a\n\
-\completely different tack.  To speed the game you can sometimes move long\n\
-\distances with a single word.  For example, 'building' usually gets you to\n\
-\the building from anywhere above ground except when lost in the forest.\n\
-\Also, note that cave passages turn a lot, and that leaving a room to\n\
-\the north does not guarantee entering the next from the south.  Good luck!"
+helpString = "To move, try words like left and right.\n\
+\To start the game, hit any key. Once in the game\n\
+\you cannot leave until you run out of turns or lose. Good luck!"
 
 
 -- This is just the intro string that is partially from https://www.amc.com/shows/halt-and-catch-fire/exclusives/colossal-cave-adventure
 gameIntro = "              Welcome to Colossal Haskell Adventure\n\
-            \     Based on Colossal Cave Adventure created by Will Crowther\n\n\
+            \     Based slightly on Colossal Cave Adventure created by Will Crowther\n\n\
             \To play the game, type short phrases into the command line below.\n\ 
-            \If you type the word 'look,' the game gives you a description of your surroundings.\n\
+            \If you type the word 'quit,' you can leave the game, but only from this menu.\n\
             \Typing 'inventory' tells you what you're carrying.\n\
-            \'Get' 'drop' and 'throw' helps you interact with objects.\n\
             \Part of the game is trying out different commands and seeing what happens.\n\
             \Type 'help' at any time for game instructions.\n\n\
-            \Would you like more instructions?"
+            \To start the game, hit enter and to quit the game type quit.\n\
+            \ YOU CANNOT QUIT once the game begins. You get 5 trys to enter the cave. Good Luck!"
 
