e08351bc331c6e8433a302a3a33f20675e3e0a66
Paul Hubbard <phubbard@pdx.edu>
2020-03-15 21:25:03 -0700

    Game is done. Might add some more rooms

--- FinalProject/ColossalCaveAdventure/app/Main.hs
+++ FinalProject/ColossalCaveAdventure/app/Main.hs
@@ -14,21 +14,25 @@ winString = "You are at the cave and have found the key! Congratulations, you no
 
 
 -- This is just the string for help that is from https://www.amc.com/shows/halt-and-catch-fire/exclusives/colossal-cave-adventure
-helpString = "To move, try words like left and right.\n\
-\To start the game, hit any key. Once in the game\n\
-\you cannot leave until you run out of turns or lose. Good luck!"
+helpString = "To move, try words like left, right, east and west.\n\
+\To quit to the main menu type 'quit'.\n\
+\Typing 'inventory' tells you what you're carrying.\n\
+\Typing 'location' tells you where you are currently.\n\
+\The entire point is to get to the cave in the least amount of moves.\n\
+\Good luck!\n"
 
 
 -- This is just the intro string that is partially from https://www.amc.com/shows/halt-and-catch-fire/exclusives/colossal-cave-adventure
-gameIntro = "              Welcome to Colossal Haskell Adventure\n\
+gameIntro = "              Welcome to Not So Colossal Haskell Adventure\n\
             \     Based slightly on Colossal Cave Adventure created by Will Crowther\n\n\
             \To play the game, type short phrases into the command line below.\n\ 
-            \If you type the word 'quit,' you can leave the game, but only from this menu.\n\
+            \If you type the word 'quit,' during the game you can get back to this menu.\n\
             \Typing 'inventory' tells you what you're carrying.\n\
+            \Typing 'location' tells you where you are currently.\n\
             \Part of the game is trying out different commands and seeing what happens.\n\
             \Type 'help' at any time for game instructions.\n\n\
-            \To start the game, hit enter and to quit the game type quit.\n\
-            \ YOU CANNOT QUIT once the game begins. You get 5 trys to enter the cave. Good Luck!"
+            \To start the game, HIT ENTER and to quit the game type QUIT.\n\
+            \ Type HELP at any time to get more instructions. Good Luck!\n"
 
 -- ////////////////////////////end Vars
 
@@ -48,9 +52,9 @@ main =  do
         response <- getLine
         if response /= "quit"
                 then do
-                        putStrLn "You've been told there is gold in a cave near by.\n You are next to a river and a house."
+                        putStrLn "You've been told there is gold in a cave near by,\nand you are on a road that seems to stretch on forever \nnext to a river with salmon that smile at you \nand a valley that makes you re-think your life choices.\n"
                         let inventory = "none"
-                        game (playerPos , inventory)
+                        game (playerPos , 0, inventory)
                         main
                 else return()
 
@@ -60,36 +64,38 @@ main =  do
 clear = putStr "\ESC[2J"
 
 -- this is the map for the game
-gameMap = [ (("road", "none") , (1, 2)) , (("river", "none") , (3, 0)) , (("house", "key") , (0, 1)), (("cave", "door") , (1, 0))]
+gameMap = [ (("road that seems to stretch on forever", "none") , (1, 4)) , (("river with salmon that smile at you", "none") , (2, 0)) , (("sad looking house", "key") , (4, 1)), (("cave (probably the one with gold)", "door") , (1, 0)) , (("valley that makes you re-think your life choices", "none") , (0, 5)) , (("liquor store for dragons", "none") , (3, 6)) , (("big and tall robe store for plus size wizards", "none") , (5, 1))]
 
 
 -- This command processes the user input and returns the next location
-processCommand :: (String , Maybe Int, Int) -> ((String , String) , (Int , Int))
-processCommand (a , c, b) 
-                        | a == "help" = ((helpString , "none") , (-1, -1))
+processCommand :: (String , Maybe Int, Int, String) -> ((String , String) , (Int , Int))
+processCommand (a , c, b, d) 
+                        | (map toUpper a) == "HELP" = ((helpString , "none") , (-1, -1))
+                        | (map toUpper a) == "INVENTORY" = ((getInv d, "none"), (-1, -1))
+                        | a == "location" = ((("You are at the " ++ getCurrLoc (b) ++ "\nand next to you is a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b)), getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
                         | b == -2 = (("Sorry, that command is not recognized -2" , "none") , (-1, -1))
-                        | isInputValid (a, b) = if getMapItem (b) == "none" then ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
-                                        else if getMapItem(b) == "key" then ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b) ++ " and you have found a " ++ getMapItem(b) ++ " in the house.") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                        | isInputValid (a, b) = if getMapItem (b) == "none" then ((("You are at the " ++ getCurrLoc (b) ++ "\nand next to you is a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else if getMapItem(b) == "key" then ((("You are next to a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b) ++ "\nand you have found a " ++ getMapItem(b) ++ " in the house.") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
                                         else if getMapItem(b) == "door" && c /= Nothing then (((winString) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
-                                        else if getMapItem(b) == "door" && c == Nothing then ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b) ++ ". You see a key hole, but have no key. It must be somewhere...") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
-                                        else ((("You are next to a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else if getMapItem(b) == "door" && c == Nothing then ((("You are next to a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b) ++ ".\nYou see a key hole in the door to the cave, \nbut have no key. It must be somewhere...") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else ((("You are at the " ++ getCurrLoc (b) ++ " and next to you is a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
                         | otherwise = (("Sorry, that command is not recognized" , "none") , (-1, -1))
 
                                         
 -- This is the main game function
 -- It recurses passing in the users postion in the game map
 -- and the users inventory. 
-game :: (Int , String) -> IO()
-game (pos , inv) = do
+game :: (Int , Int, String) -> IO()
+game (pos , turns, inv) = do
         response <- getLine
         when (response /= "quit")
                 $ do
                         let nextPos = (getNextLocation (response ,  pos)) --try two
-                        let location = (processCommand (response , findString "key" inv, nextPos)) --try one
-                        putStrLn (fst ( fst (location)))
+                        let location = (processCommand (response , findString "key" inv, nextPos, inv)) --try one
+                        putStrLn ((fst ( fst (location))) ++ "\n Turn Number: " ++ show turns ++ "\n")
                         let inventory = (snd (fst (location))) ++ inv 
                         if ((fst ( fst (location))) /= winString) then do
-                                game (nextPos , inventory)
+                                game (nextPos , (turns + 1), inventory)
                         else do
                                 wait <- getLine
                                 return()
@@ -115,26 +121,43 @@ getMapRightLoc a = (snd (snd (gameMap !! a)))
 getMapItem :: Int -> String
 getMapItem a = snd (fst ((gameMap !! a)))
 
+-- returns the current location string
+getCurrLoc :: Int -> String
+getCurrLoc a = fst ( fst(gameMap !! a))
+
+-- returns the key if in inventory, empty if not
+getInv :: String -> String
+getInv a | findString "key" a /= Nothing = "you have a key"
+         | otherwise = "Pockets are empty, but hopes are high."
+
 -- This returns the next list location from the string input by the user
 getNextLocation :: (String , Int) ->  Int
 getNextLocation (a , b) = if (map toUpper a) == "LEFT"  then (getMapLeftLoc(b))
-                    else if (map toUpper a) == (map toUpper (getMapLeft(b))) then (getMapLeftLoc(b))
-                    else if (map toUpper a) == (map toUpper (getMapRight(b))) then (getMapRightLoc(b))
+                    else if (map toUpper a) == "EAST" then (getMapLeftLoc(b))
+                    else if (map toUpper a) == "WEST" then (getMapRightLoc(b))
                     else if (map toUpper a) == "RIGHT" then (getMapRightLoc(b))
-                    else -2
+                    else b
 
 -- This returns true if the user input is a valid input
 isInputValid :: (String, Int) -> Bool
 isInputValid (a, b) | (map toUpper a) == "LEFT" = True
-                    | (map toUpper a) == (map toUpper (getMapLeft(b))) = True
-                    | (map toUpper a) == (map toUpper (getMapRight(b))) = True
+                    | (map toUpper a) == "EAST" = True
+                    | (map toUpper a) == "WEST" = True
                     | (map toUpper a) == "RIGHT" = True
                     | (map toUpper a) == "NONE" = True
                     | otherwise = False
 
 
--- TODO: Add a function that shows your location so you can tell the 
---       player where they are. Add that to the string from processCommand.
---       Edit the help and beginning output. 
---       Test the isInputValid with the actual location names. Get it to work. 
+-- Test to see that the maze is solvable
+-- $ stack run
+-- press 'enter' key
+-- $ left
+-- $ left
+-- $ right
+-- $ left
+-- $ left
+-- $ right
+-- $ left
+-- press 'enter' key
+-- $ quit
 
