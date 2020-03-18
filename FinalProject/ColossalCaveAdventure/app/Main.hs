2cc8443c898f431cc171c84bafb20cde2015f27e
Paul Hubbard <phubbard@pdx.edu>
2020-03-17 18:59:01 -0700

    Added items and new end states.

--- FinalProject/ColossalCaveAdventure/app/Main.hs
+++ FinalProject/ColossalCaveAdventure/app/Main.hs
@@ -64,7 +64,7 @@ main =  do
 clear = putStr "\ESC[2J"
 
 -- this is the map for the game
-gameMap = [ (("road that seems to stretch on forever", "none") , (1, 4)) , (("river with salmon that smile at you", "none") , (2, 0)) , (("sad looking house", "key") , (4, 1)), (("cave (probably the one with gold)", "door") , (1, 0)) , (("valley that makes you re-think your life choices", "none") , (0, 5)) , (("liquor store for dragons", "none") , (3, 6)) , (("big and tall robe store for plus size wizards", "none") , (5, 7)), (("Betty White secret fight club night club", "none") , (6, 8)) , (("mullet festival", "none") , (6, 4))]
+gameMap = [ (("road that seems to stretch on forever", "none") , (1, 4)) , (("river with salmon that smile at you", "none") , (2, 0)) , (("sad looking house", "key") , (4, 1)), (("cave (probably the one with gold)", "door") , (1, 0)) , (("valley that makes you re-think your life choices", "none") , (0, 5)) , (("liquor store for dragons", "dragon") , (3, 6)) , (("big and tall robe store for plus size wizards", "none") , (5, 7)), (("Betty White secret fight club night club", "hair") , (6, 8)) , (("mullet festival", "none") , (6, 4))]
 
 
 -- This command processes the user input and returns the next location
@@ -75,7 +75,9 @@ processCommand (a , c, b, d)
                         | (map toUpper a) == "LOCATION" = ((("You are at the " ++ getCurrLoc (b) ++ "\nand next to you is a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b)), getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
                         | b == -2 = (("Sorry, that command is not recognized -2" , "none") , (-1, -1))
                         | isInputValid (a, b) = if getMapItem (b) == "none" then ((("You are at the " ++ getCurrLoc (b) ++ "\nand next to you is a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
-                                        else if getMapItem(b) == "key" then ((("You are next to a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b) ++ "\nand you have found a " ++ getMapItem(b) ++ " in the house.") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else if getMapItem(b) == "key" then ((("You are next to a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b) ++ "\nand you have found a " ++ getMapItem(b) ++ " in the house. (limit one per person)") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else if getMapItem(b) == "hair" then ((("You are next to a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b) ++ "\nand you have found a lock of Betty White's hair.\nWith this magic item, you might get an extra prize. (limit one per person)") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
+                                        else if getMapItem(b) == "dragon" then ((("You are next to a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b) ++ "\nand you have found a bottle of 'Dragons Gold'.\nSeems to be a worse version of a liquor with gold flakes in it (if it could be worse).\nWith this magic item, you might get an extra prize. (limit one per person)") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
                                         else if getMapItem(b) == "door" && c /= Nothing then (((winString) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
                                         else if getMapItem(b) == "door" && c == Nothing then ((("You are next to a " ++ getMapLeft (b) ++ "\nand a " ++ getMapRight(b) ++ ".\nYou see a key hole in the door to the cave, \nbut have no key. It must be somewhere...") , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
                                         else ((("You are at the " ++ getCurrLoc (b) ++ " and next to you is a " ++ getMapLeft (b) ++ " and a " ++ getMapRight(b)) , getMapItem(b)) , (getMapLeftLoc(b), getMapRightLoc(b)))
@@ -90,15 +92,36 @@ game (pos , turns, inv) = do
         response <- getLine
         when (response /= "quit")
                 $ do
-                        let nextPos = (getNextLocation (response ,  pos)) --try two
-                        let location = (processCommand (response , findString "key" inv, nextPos, inv)) --try one
+                        let nextPos = (getNextLocation (response ,  pos)) 
+                        let location = (processCommand (response , findString "key" inv, nextPos, inv)) 
                         putStrLn ((fst ( fst (location))) ++ "\n Turn Number: " ++ show turns ++ "\n")
                         let inventory = (snd (fst (location))) ++ inv 
                         if ((fst ( fst (location))) /= winString) then do
                                 game (nextPos , (turns + 1), inventory)
                         else do
-                                wait <- getLine
-                                return()
+                                if (hasHair inv && hasLiquor inv) then do
+                                                                putStrLn "Extra Credit:\nYou have the Dragon's Gold liquor! That slowed down time for you, and gave you a better score!\nYou have the Betty White's Hair! The magic of Betty White has turned back time, and gave you a better score!"
+                                                                let newTurnsTwo = turns - 3;
+                                                                putStrLn ("\nFinal Score: " ++ show newTurnsTwo)
+                                                                wait <- getLine
+                                                                return()
+                                else if hasLiquor inv then do 
+                                                        putStrLn "Extra Credit:\nYou have the Dragon's Gold liquor! That slowed down time for you, and gave you a better score!\nLooks like you didn't visit the Betty White Night Club Fight Club. Oh well..."
+                                                        let newTurns = turns - 1;
+                                                        putStrLn ("\nFinal Score: " ++ show newTurns)
+                                                        wait <- getLine
+                                                        return()
+                                else if hasHair inv then do
+                                                        putStrLn "Extra Credit:\nYou have a lock Betty White's Hair! The magic of Betty White has turned back time, and gave you a better score!\nLooks like you didn't go the the Dragon Liquor Store. Oh well..."
+                                                        let newTurnsOne = turns - 2;
+                                                        putStrLn ("\nFinal Score: " ++ show newTurnsOne)
+                                                        wait <- getLine
+                                                        return()
+                                else do 
+                                        putStrLn "Extra Credit:\nLooks like you didn't get the Dragon's Liquor or Betty White's hair. Oh well."
+                                        putStrLn ("Final Score: " ++ show turns)
+                                        wait <- getLine
+                                        return()
                 
 
 -- returns the left location string
@@ -127,8 +150,12 @@ getCurrLoc a = fst ( fst(gameMap !! a))
 
 -- returns the key if in inventory, empty if not
 getInv :: String -> String
-getInv a | findString "key" a /= Nothing = "you have a key"
-         | otherwise = "Pockets are empty, but hopes are high."
+getInv a | (findString "key" a /= Nothing) && (findString "dragon" a /= Nothing) &&  (findString "hair" a /= Nothing)= "\nInventory:\nCave key\nBottle of Dragon's Gold liquor\nA lock of Betty White's hair"
+         | (findString "key" a /= Nothing) && (findString "dragon" a /= Nothing) = "\nInventory:\nCave key\nBottle of Dragon's Gold liquor"
+         | findString "key" a /= Nothing = "\nInventory:\nA cave key"
+         | findString "dragon" a /= Nothing = "\nInventory:\nA bottle of Dragon's Gold liquor"
+         | findString "hair" a /= Nothing = "\nInventory:\nA lock of Betty White's hair"
+         | otherwise = "\nPockets are empty, but hopes are high."
 
 -- This returns the next list location from the string input by the user
 getNextLocation :: (String , Int) ->  Int
@@ -147,13 +174,20 @@ isInputValid (a, b) | (map toUpper a) == "LEFT" = True
                     | (map toUpper a) == "NONE" = True
                     | otherwise = False
 
+-- checks for the dragon liquor
+hasLiquor :: String -> Bool
+hasLiquor a | findString "dragon" a /= Nothing = True
+            | otherwise = False
+
+-- checks for the Betty Whites Hair
+hasHair :: String -> Bool
+hasHair a | findString "hair" a /= Nothing = True
+            | otherwise = False
 
 -- Test to see that the maze is solvable and game has an end state. 
 -- $ stack run
 -- press 'enter' key
 -- $ left
--- $ left
--- $ right
 -- $ left
 -- $ left
 -- $ right
@@ -187,4 +221,15 @@ isInputValid (a, b) | (map toUpper a) == "LEFT" = True
 -- Test that you need the key to win
 -- stack run
 -- hit 'enter' key
--- 
+-- $ right 
+-- $ right 
+-- $ left : Should see that you can't enter the cave
+-- $ right 
+-- $ left
+-- $ left
+-- $ inventory : Should see you have the key
+-- $ left
+-- $ right
+-- $ left
+-- press 'enter' key
+-- $ quit
